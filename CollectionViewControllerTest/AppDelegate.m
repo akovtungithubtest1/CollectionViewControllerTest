0e00a8f3c9e08c4a8a0e834ece1deabb63a934de
a-kovtun <oleksiykovtun@server.local>
2015-02-19 14:30:18 +0200

    Fixed build errors by adding #import "AppDelegate.h" to main.m and AppDelegate.m.

--- CollectionViewControllerTest/AppDelegate.m
+++ CollectionViewControllerTest/AppDelegate.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Yanpix. All rights reserved.
 //
 
+#import "AppDelegate.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
